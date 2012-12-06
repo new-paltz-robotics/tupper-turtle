dfb4a3f415308cb235558a161a0c1fd42bbff40b
Kevin <pieratemaster@aim.com>
2012-12-05 19:01:13 -0500

    ITS ALIVE. and is a cat. senses the light, turns towards it and stops

--- SeeTheLIGHT.ino
+++ SeeTheLIGHT.ino
@@ -3,6 +3,9 @@ int rDirPin = 12; //12
 int rBreakPin = 9; //9
 int rSpeedPin = 3; //3
 
+int RIGHT = 1;
+int LEFT = 0;
+
 int lDirPin = 13; //13
 int lBreakPin = 8; //8
 int lSpeedPin = 11; //11
@@ -11,18 +14,53 @@ int lSpeedPin = 11; //11
 void setup()
 {
   Serial.begin(9600);          //  setup serial
+  
+  pinMode(rDirPin, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(rBreakPin, OUTPUT); //Initiates Brake Channel A pin
+
+  //Setup Channel B
+  pinMode(lDirPin, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(lBreakPin, OUTPUT);  //Initiates Brake Channel A pin
+  
 }
 
 void loop()
 {
   int variance = sensorMethod();    // read the input pin
   Serial.println(variance);    // debug value
-  if (variance >0)
+  if (variance >0){
     Serial.print("right");
-  else
+    turn(RIGHT, variance);
+  }
+  else{
+    turn(LEFT, (-1* variance));
     Serial.print("left");
+  }
 }
 
+
+void turn(int dir, int magnitude){
+ if (dir == LEFT) {
+   digitalWrite(rDirPin, HIGH);
+   digitalWrite(rBreakPin, LOW);
+   analogWrite(rSpeedPin, magnitude);
+   
+   digitalWrite(lDirPin, HIGH); 
+   digitalWrite(lBreakPin, LOW); //Disengage the Brake for Channel B
+   analogWrite(lSpeedPin, magnitude); 
+ }
+ if (dir == RIGHT){
+   digitalWrite(rDirPin, LOW);
+   digitalWrite(rBreakPin, LOW);
+   analogWrite(rSpeedPin, magnitude);
+   
+   digitalWrite(lDirPin, LOW); 
+   digitalWrite(lBreakPin, LOW); //Disengage the Brake for Channel B
+   analogWrite(lSpeedPin, magnitude); 
+ }
+ 
+ 
+}
 
 int sensorMethod(){
  //returns between +/-512 
